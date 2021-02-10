0c185b8617279484cf976862d1e8116ac7ed6453
Dawid <dawidbury1998@gmail.com>
2021-02-10 19:19:43 +0100

    Change layout

--- src/actions/authActions.js
+++ src/actions/authActions.js
@@ -1,45 +1,50 @@
-import axios from 'axios';
-import apiUrl from '../config/env';
+import axios from "axios";
+import apiUrl from "../config/env";
 
 export const signOut = () => {
-    return (dispatch, getState) => {
-        dispatch({ type: 'SIGNOUT_SUCCESS' })
-        dispatch({ type: 'REGISTER_NOTIFICATION_SWITCH', payload: true, text: `Zostałeś wylogowany!` });
-    };
-}
-
+  return (dispatch, getState) => {
+    dispatch({ type: "SIGNOUT_SUCCESS" });
+    dispatch({
+      type: "REGISTER_NOTIFICATION_SWITCH",
+      payload: true,
+      text: `Zostałeś wylogowany!`,
+    });
+  };
+};
 
 export const signUp = (newUser) => {
-    return (dispatch, getState) => {
-        axios.post(`${apiUrl}/users/register`, {
-            'email': newUser.email,
-            'password': newUser.password,
-            'phone': newUser.phone,
-            'firstName': newUser.firstName,
-            'lastName': newUser.lastName,
-        }).then(function (res) {
-                dispatch({ type: 'SIGNUP_SUCCESS', res });
-                return true
-            }).catch(function (err) {
-                console.log(err);
-                return false
-            });
-    }
-}
-
+  return (dispatch, getState) => {
+    axios
+      .post(`${apiUrl}/users/register`, {
+        email: newUser.email,
+        password: newUser.password,
+        phone: newUser.phone,
+        firstName: newUser.firstName,
+        lastName: newUser.lastName,
+      })
+      .then(function (res) {
+        dispatch({ type: "SIGNUP_SUCCESS", res });
+        return true;
+      })
+      .catch(function (err) {
+        return false;
+      });
+  };
+};
 
 export const login = (user) => {
-    return (dispatch, getState) => {
-        axios.post(`${apiUrl}/users/login`, {
-            'email': user.email,
-            'password': user.password,
-        }).then(function (res) {
-                dispatch({ type: 'LOGIN_SUCCESS', res });
-                return true
-            }).catch(function (err) {
-                console.log(err);
-                return false
-            });
-    }
-}
-
+  return (dispatch, getState) => {
+    axios
+      .post(`${apiUrl}/users/login`, {
+        email: user.email,
+        password: user.password,
+      })
+      .then(function (res) {
+        dispatch({ type: "LOGIN_SUCCESS", res });
+        return true;
+      })
+      .catch(function (err) {
+        return false;
+      });
+  };
+};
